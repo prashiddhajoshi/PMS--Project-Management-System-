54e2d58f432969e79a9e7e4c275fd09f427fb18f
Prashiddha <prashiddha.joshi@sproutify.com>
2011-03-28 13:49:14 +0545

    Initial stuffs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManagementSystem::Application.config.secret_token = '53eda6669cda80bf28542cd5bef3e78cde47834d3bcc8de6b66a122ce2a1b3676a72ea263e2327570c454831a742431e7e48764dffdc49b9cf84bfcd4061e00c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectManagementSystem::Application.config.session_store :cookie_store, :key => '_Project_Management_System_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProjectManagementSystem::Application.config.session_store :active_record_store
