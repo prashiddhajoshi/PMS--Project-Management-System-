ed0b7cdfee8097301ce31a1060bf3b645ef71ce8
Prashiddha <prashiddha.joshi@sproutify.com>
2011-03-29 12:09:13 +0545

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ ProjectManagementSystem::Application.routes.draw do
     resources :tasks
   end
 
+  root :to => "projects#index"
+
+  # root :to => "dashboard#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -57,7 +61,7 @@ ProjectManagementSystem::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  #root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
